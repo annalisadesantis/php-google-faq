1be76bedca4d6945b5d366da07905aabfec31e2f
Annalisa <annalisadesantis770@gmail.com>
2020-12-16 15:20:12 +0100

    stampa in pagina delle domande e risp

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ $domande = [
         "risp1" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ]
 ];
-var_dump($domande);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -42,7 +41,17 @@ var_dump($domande);
         </header>
         <!-- Main -->
         <main>
-
+            <div class="container">
+                <?php
+                    foreach ($domande as $domanda => $risposte) { ?>
+                        <h2> <?php echo $domanda ?></h2>
+                        <?php
+                        foreach ($risposte as $risposta) { ?>
+                            <p> <?php echo $risposta; ?> </p>
+                        <?php
+                        }
+                    } ?>
+            </div>
         </main>
         <!-- Footer -->
         <footer>

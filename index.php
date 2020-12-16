869131532cc59a7bb2dc71f925c6efa3d884fd5d
Annalisa <annalisadesantis770@gmail.com>
2020-12-16 14:32:25 +0100

    creazione file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- FAVICON -->
+        <link rel="icon" href="https://ssl.gstatic.com/policies/favicon.ico">
+        <!-- CSS -->
+        <link rel="stylesheet" href="dist/style.css">
+        <!-- GOOGLE FONT -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>ciao</h1>
+    </body>
+</html>

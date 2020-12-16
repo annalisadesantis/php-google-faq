892133a7bac1cf9d32313e5069c655a40b5d7336
Annalisa <annalisadesantis770@gmail.com>
2020-12-16 16:36:50 +0100

    fine header

--- index.php
+++ index.php
@@ -20,6 +20,9 @@ $domande = [
         "risp1" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ]
 ];
+
+$nav = ["Introduzione", "Norme sulla privacy", "Termini di servizio", "Tecnologie", "Domande frequenti"];
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -37,7 +40,25 @@ $domande = [
     <body>
         <!-- Header -->
         <header>
-
+            <div class="title">
+                <div class="logo">
+                    <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="logo">
+                </div>
+                <div class="privacy">
+                    <span>Privacy e Termini</span>
+                </div>
+            </div>
+            <div class="nav">
+                <ul>
+                    <?php
+                    for ($i=0; $i < count($nav); $i++) { ?>
+                        <li>
+                            <?php echo $nav[$i]; ?>
+                        </li>
+                    <?php
+                    } ?>
+                </ul>
+            </div>
         </header>
         <!-- Main -->
         <main>

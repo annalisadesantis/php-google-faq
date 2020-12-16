459143f5a9f38f92fe5921817c214bb447a4eb9c
Annalisa <annalisadesantis770@gmail.com>
2020-12-16 15:12:03 +0100

    aggiunta partials

--- index.php
+++ index.php
@@ -36,6 +36,17 @@ var_dump($domande);
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     </head>
     <body>
+        <!-- Header -->
+        <header>
 
+        </header>
+        <!-- Main -->
+        <main>
+
+        </main>
+        <!-- Footer -->
+        <footer>
+
+        </footer>
     </body>
 </html>
